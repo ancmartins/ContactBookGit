22b6b64b00026d1225a12be71f663a08a9e451ff
mmnogueiro71227 <mm.nogueiro@campus.fct.unl.pt>
2026-09-19 20:31:18 +0100

    fix: Removed comments from previous logic in hasRepeatNumbers method

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -100,22 +100,6 @@ public class ContactBook {
         contacts = tmp;
     }
 
-//    public boolean hasRepeatNumbers() {
-//        int i = 0;
-//        boolean repeated = false;
-//        while(i < counter && !repeated){
-//            int phone = contacts[i].getPhone();
-//            int j = i + 1;
-//            while(j < counter && !repeated){
-//                if(contacts[j].getPhone() == phone){
-//                    repeated = true;
-//                } else {j++;}
-//            }
-//            i++;
-//        }
-//        return repeated;
-//    }
-
     public boolean hasRepeatNumbers() {
         for(int i = 0; i < counter; i++){
             for(int j = i + 1; j < counter; j++){
